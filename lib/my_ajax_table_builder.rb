5bb72edeb1b9cfa64011798789c4b97b7558bd24
Don Isaac <donald.isaac@gmail.com>
2019-01-23 14:17:54 -0500

    minor changes to ransack table one/two

--- lib/my_ajax_table_builder.rb
+++ lib/my_ajax_table_builder.rb
@@ -4,6 +4,7 @@ class MyAjaxTableBuilder < Grapple::AjaxDataGridBuilder
 	# Cycle row colors
 	configure :body, tr: Proc.new { |template| { :class => template.cycle('even', 'odd') } }
 	helper :ransack_form, Grapple::Components::RansackSearchForm
+	helper :ransack_query, Grapple::Components::RansackQueryField
 	
 	# To enable history by default
 	#self.container_attributes(template, options)
